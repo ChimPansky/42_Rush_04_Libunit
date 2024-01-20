75ecd5ec4ef0a88ada9dd283065a30412af59b09
Simon Dablander <sdabland@student.42vienna.com>
2024-01-20 15:21:28 +0100

    feat: logging & basic executor

--- framework/libunit.h
+++ framework/libunit.h
@@ -2,8 +2,23 @@
 # define LIBUNIT_H
 
 # include "libft.h"
+# include <stdlib.h>
 # include <sys/types.h>
 # include <sys/wait.h>
+
+# define DEF_COLOR "\033[0;39m"
+# define GRAY "\033[0;90m"
+# define RED "\033[0;91m"
+# define GREEN "\033[0;92m"
+# define YELLOW "\033[0;93m"
+# define BLUE "\033[0;94m"
+# define MAGENTA "\033[0;95m"
+# define CYAN "\033[0;96m"
+# define WHITE "\033[0;97m"
+
+# ifndef TEST_TIMEOUT_SECS
+#  define TEST_TIMEOUT_SECS 3
+# endif
 
 # define SUCCESS 0
 # define FAILURE -1
@@ -15,6 +30,19 @@ typedef struct s_test_case
 }				t_test_case;
 
 typedef void t_unit_test;
+
+typedef	enum e_test_status
+{
+	STATUS_OK,
+	STATUS_KO,
+	STATUS_SIGSEGV,
+	STATUS_SIGBUS,
+	STATUS_TIMEOUT,
+	STATUS_NOT_RUN,
+	STATUS_UNKNOWN
+}				t_test_status;
+
+void	log_test(char *launcher_name, t_unit_test *test, t_test_status status);
 
 t_test_case	*test_case_add(t_unit_test **tests, char *title, void *test_function);
 
---
+++ framework/src/logging.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   logging.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdabland <sdabland@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 13:59:41 by sdabland          #+#    #+#             */
+/*   Updated: 2024/01/20 15:05:03 by sdabland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+
+static char	*get_status_str(t_test_status status)
+{
+	if (status == STATUS_OK)
+		return (GREEN "OK" DEF_COLOR);
+	else if (status == STATUS_KO)
+		return (RED "KO" DEF_COLOR);
+	else if (status == STATUS_NOT_RUN)
+		return (GRAY "SKIPPED" DEF_COLOR);
+	else if (status == STATUS_SIGSEGV)
+		return (YELLOW "SIGSEGV" DEF_COLOR);
+	else if (status == STATUS_SIGBUS)
+		return (YELLOW "SIGBUS" DEF_COLOR);
+	else if (status == STATUS_TIMEOUT)
+		return (MAGENTA "TIMEOUT" DEF_COLOR);
+	return (CYAN "UNKNOWN" DEF_COLOR);
+}
+
+void	log_test(char *launcher_name, t_unit_test *test, t_test_status status)
+{
+	(void) test;
+	ft_putstr_fd(launcher_name, STDOUT_FILENO);
+	ft_putstr_fd(": ", STDOUT_FILENO);
+	// ft_putstr_fd(test->name, STDOUT_FILENO);
+	ft_putstr_fd("HEllo", STDOUT_FILENO);
+	ft_putstr_fd(": ", STDOUT_FILENO);
+	ft_putstr_fd("[", STDOUT_FILENO);
+	ft_putstr_fd(get_status_str(status), STDOUT_FILENO);
+	ft_putstr_fd("]", STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+}
