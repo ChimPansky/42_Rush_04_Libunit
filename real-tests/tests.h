c1cc77ae96f58a900a6cb20e8a6d18401761ba38
Thomas Kasbarian <tkasbari@c2r5p12.42vienna.com>
2024-01-20 15:05:25 +0100

    linked list stuff

---
+++ real-tests/tests.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkasbari <thomas.kasbarian@gmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 13:32:01 by sdabland          #+#    #+#             */
+/*   Updated: 2024/01/20 14:41:53 by tkasbari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTS_H
+# define TESTS_H
+
+# include "libunit.h"
+# include "libft.h"
+
+int	ft_isalpha_launcher(void);
+int test_lower_case(void);
+int test_upper_case(void);
+int test_digit(void);
+int test_special(void);
+int test_null_terminator(void);
+
+#endif
